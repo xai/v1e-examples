5a9f8e470c6dfab65516809485911c2157897be3
Olaf Lessenich <olaf.lessenich@wu.ac.at>
2020-10-06 10:52:42 +0200

    Add wrapper script to run examples and have a nicer shell

---
+++ run.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+if [ -z "$1" ]
+then
+	echo "Usage $0 <file>"
+	exit 1
+fi
+
+if [ "$1" == "-i" ]
+then
+	rlwrap -c -H .history ./djdslkit-latest-linux
+	exit $?
+elif [ "$1" == "-v" ]
+then
+	verbose=1
+	shift
+fi
+
+input="$1"
+name="$(basename $1 .tcl)"
+
+if [ ! -d dot ]
+then
+	mkdir dot
+fi
+
+echo "Running $name"
+./djdslkit-latest-linux $input | tee dot/${name}.dot
+
+if [[ $? -eq 0 && ! -z "$verbose" ]]
+then
+	dot -Tpng -o dot/${name}.png dot/${name}.dot
+	if [ $? -eq 0 ]
+	then
+		eog dot/${name}.png &
+		exit 0
+	else
+		exit 3
+	fi
+else
+	exit 2
+fi
