0617d7d7272bf0ff49fc3ac5a9cd2777169bd0cc
Olaf Lessenich <olaf.lessenich@wu.ac.at>
2020-10-21 18:23:00 +0200

    Add at-least k and combined example

---
+++ 2_n.tcl
@@ -0,0 +1,16 @@
+source /home/xai/src/djdsl/v1e/library/v1e.tcl
+
+package require djdsl::v1e
+namespace import ::djdsl::v1e::*
+
+set gpl [Model newFromScript {
+  Root "Graph" {
+    Choice -lower 2 -upper 3 {
+      Feature -name "coloured"
+      Feature -name "weighted"
+      Feature -name "directed"
+    }
+  }
+}]
+
+puts [$gpl asDot]
